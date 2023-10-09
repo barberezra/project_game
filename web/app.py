5e667434582fd52e636e7ef813a45c71acb8950c
TetraGenesis <santmonbarb@gmail.com>
2023-10-09 11:36:26 -0500

    added favicon for no reason

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_from_directory
+import os
 app = Flask(__name__, template_folder='./templates')
 
 @app.route('/')
