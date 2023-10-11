4ad4c1b509672b4179b1067647a34ff9c3955448
TetraGenesis <santmonbarb@gmail.com>
2023-10-11 15:46:55 -0500

    Tentative game logic starting point

--- web/app.py
+++ web/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, jsonify
 import os
 app = Flask(__name__, template_folder='./templates')
 
 @app.route('/')
 def load():
     return render_template('index.html')
+
+@app.route('/capture_pit', methods=['POST'])
+def capture_pit():
+    data = request.get_json()
+    pit = data.get('pit')
+    # GAME LOGIC GOES HERE
+    return jsonify({'pit': pit})
