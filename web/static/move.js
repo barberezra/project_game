2a04d845b5bd1b7a8a6be435e97c7cd9d4a15767
TetraGenesis <santmonbarb@gmail.com>
2023-10-18 16:55:29 -0500

    Fixed value capture

--- web/static/move.js
+++ web/static/move.js
@@ -1,14 +1,3 @@
-function range(start, end) {
-    var result = [];
-    for (let i = start; i <= end; i++) {
-        result.push(i)
-    }
-    return result;
-}
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.pit').forEach(function(pit) {
         var pitNumber = pit.getAttribute('data-pit');
@@ -16,13 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
         if (pitNumber !== "7" && pitNumber !== "14") {
             pit.addEventListener('click', function() {
                 // Get the pit number from the data attribute
-                var pitNumber = pit.getAttribute('data-pit'); // FIX DATA GATHERING (LIST LOGIC)
-                var nextPit = (pitNumber+1) % 14
-                const pitRange = range(nextPit, nextPit + (pit.textContent-1))
-                var pitValues= []
-                for (pitValue in pitRange) {
-                    pitValues.push(pit.textContent)
+                var pitNumber = parseInt(pit.getAttribute('data-pit'));
+                var nextPit = (pitNumber + 1) % 14;
+                var numPitsToCollect = parseInt(pit.textContent);
+                var pitNumbers = [];
+                var pitValues = [];
+
+                for (var i = 0; i < numPitsToCollect; i++) {
+                    pitNumbers.push(nextPit + i);
                 }
+
+                pitNumbers.forEach(pitIndex => {
+                    var pitElement = document.querySelector('.pit[data-pit="' + pitIndex + '"]');
+                    if (pitElement) {
+                        pitValues.push(pitElement.textContent);
+                    }
+                });
+
 
                 // Send an AJAX request to the server to capture the pit value
                 fetch('/capture_pit', {
