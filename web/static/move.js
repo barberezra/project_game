decbcd373b203f553b140fc494d0f10ab06b7002
Zoey La <zoeyla24@gmail.com>
2023-10-31 02:42:14 -0500

    ????? im gonna cry

--- web/static/move.js
+++ web/static/move.js
@@ -6,15 +6,18 @@ let currentPlayerIndex = 0;
 
 // switch player turns
 function switchTurn() {
-    currentPlayerIndex = (currentPlayerIndex + 1) % 2;
-    document.querySelector('h3').innerHTML = `Welcome! ${players[currentPlayerIndex]}, it's your turn.`;
+  currentPlayerIndex = (currentPlayerIndex + 1) % 2;
 }
 
 // finds the range of affected pits from click dispersal
 function range(start, moves) {
-    let res = [];
+    var res = [];
     for (let i = 1; i <= parseInt(moves); i++) {
-        res.push((parseInt(start) + i) % 14 || 14);
+        if ((parseInt(start) + i) == 14) {
+            res.push(14);
+        } else {
+            res.push((parseInt(start) + i) % 14);
+        }
     }
     return res;
 }
@@ -31,9 +34,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // tracking pit changes
     document.querySelectorAll('.pit').forEach(function(pit) {
         // Get the pit number from the data attribute
-        let pitNumber = parseInt(pit.getAttribute('data-pit'));
+        var pitNumber = pit.getAttribute('data-pit');
         
-        if (pitNumber !== 7 && pitNumber !== 14 && pit.textContent !== "0") {
+        if (pitNumber !== "7" && pitNumber !== "14" && pit.textContent !== "0") {
             pit.addEventListener('click', function() {
                 if ((currentPlayerIndex === 0 && pitNumber > 6) || (currentPlayerIndex === 1 && pitNumber < 7)) {
                     alert("Be patient. It's not your turn!");
@@ -41,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 
                 // Get list of all affected pits (from next pit to the last pit affected)
-                let pitRange = range(pitNumber, pit.textContent);
+                var pitRange = range(pitNumber, pit.textContent);
 
                 // Send an AJAX request to the server to capture the pit value
                 fetch('/capture_pit', {
@@ -51,23 +54,18 @@ document.addEventListener('DOMContentLoaded', function() {
                         'Content-Type': 'application/json'
                     }
                 })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok ' + response.statusText);
-                    }
-                    return response.json();
-                })
+                .then(response => response.json())
                 .then(data => {
                     // Handle the response from the server if needed
                     console.log('Pit value captured: ' + data.pitValue);
                     console.log('Pit values affected: ' + pitRange);
                     var pitResultElement = document.querySelector('.pit[data-pit="' + pitNumber + '"]');
-                    pitResultElement.textContent = "0";
+                    pitResultElement.textContent = data.pitValue;
                     
                     for (const pit of pitRange) {
                         var pitAffectedElement = document.querySelector('.pit[data-pit="' + pit + '"]');
                         var temp = parseInt(pitAffectedElement.textContent);
-                        pitAffectedElement.textContent = temp + 1;
+                        pitAffectedElement.textContent = parseInt(temp + 1);
                     }
 
                     // Capture mechanism starts here
@@ -88,12 +86,8 @@ document.addEventListener('DOMContentLoaded', function() {
                         // Add captured stones to the Mancala store
                         mancalaStoreElement.textContent = parseInt(mancalaStoreElement.textContent) + capturedStones + 1; // +1 for the last stone that caused the capture
                     }
+                    // Capture mechanism ends here
 
-                    // Switch turns after a move
-                    switchTurn();
-                })
-                .catch((error) => {
-                    console.error('There has been a problem with your fetch operation:', error);
                 });
             });
         }
