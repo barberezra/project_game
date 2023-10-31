fc8b89eeff707a574b347500d4962d6cf06ee868
Zoey La <zoeyla24@gmail.com>
2023-10-31 02:15:48 -0500

    capturing??

--- api/api.py
+++ api/api.py
@@ -2,27 +2,58 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Initialize a simple game state
+# Pits are numbered 1-14 from left to right from Player 1's perspective
+# Pits 7 and 14 are the Mancala stores for Player 1 and Player 2 respectively
+# This is just an example; in a real application, you would want to store this
+# state in a database or some other persistent storage
+game_state = {
+    'pits': [4] * 14
+}
+game_state['pits'][6] = 0  # Mancala store for Player 1 starts empty
+game_state['pits'][13] = 0  # Mancala store for Player 2 starts empty
+
 @app.route('/move', methods=['POST'])
 def move():
     data = request.get_json()
-    # Perform the game logic based on the data
-    pitNumber = data.get('pitNumber')
-    # pitValue = data.get('pitValue') # Replace with your actual game logic
+    pitNumber = int(data.get('pitNumber'))
     pitRange = data.get('pitRange')
 
     capture = False
     capturedStones = 0
 
-    if pitNumber <= 6:
-        if pitRange[-1] <= 6 and pitRange[-1] != pitNumber:
-            capture = True
-    else:
-        if 7 <= pitRange[-1] <= 13 and pitRange[-1] != pitNumber:
-            capture = True
-    
-    if capture:
-        opposite = 14 - pitRange[-1]
-        capturedStones = 4
+    # Distribute the stones
+    stones = game_state['pits'][pitNumber - 1]
+    game_state['pits'][pitNumber - 1] = 0
+    for pit in pitRange:
+        game_state['pits'][pit - 1] += 1
+        stones -= 1
 
-    # return the current values for those pits and update it
-    return jsonify({'pitNumber': pitNumber, 'pitValue': 0, 'pitRange': pitRange, 'capture' : capture, 'capturedStones':capturedStones})
+    # Check for capture
+    last_pit = int(pitRange[-1])
+    if (1 <= last_pit <= 6 and game_state['pits'][last_pit - 1] == 1) or \
+       (8 <= last_pit <= 13 and game_state['pits'][last_pit - 1] == 1):
+        opposite_pit = 14 - last_pit
+        capturedStones = game_state['pits'][opposite_pit - 1]
+        game_state['pits'][opposite_pit - 1] = 0
+        capture = True
+        # Add captured stones to player's Mancala store
+        if last_pit <= 6:
+            game_state['pits'][6] += capturedStones + 1
+        else:
+            game_state['pits'][13] += capturedStones + 1
+
+    # Add any remaining stones to the next pits in sequence
+    for _ in range(stones):
+        last_pit = (last_pit % 14) + 1
+        game_state['pits'][last_pit - 1] += 1
+
+    # Return the updated game state
+    return jsonify({
+        'pitNumber': pitNumber,
+        'pitValue': 0,  # This might need to be updated depending on your game logic
+        'pitRange': pitRange,
+        'capture': capture,
+        'capturedStones': capturedStones,
+        'gameState': game_state['pits']
+    })
