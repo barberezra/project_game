1c871248f2d6e31c372a639afb38b7b09e3eea42
TetraGenesis <santmonbarb@gmail.com>
2023-11-01 10:28:08 -0500

    fixed errors

--- web/static/move.js
+++ web/static/move.js
@@ -45,14 +45,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Get list of all affected pits (from next pit to the last pit affected)
                 var pitRange = range(pitNumber, pit.textContent);
 
-                pitRange.forEach(pitIndex => {
-                    var pitElement = document.querySelector('.pit[data-pit="' + pitIndex + '"]');
-                    if (pitElement) {
-                        pitValues.push(pitElement.textContent);
-                    }
-                });
-
-
                 // Send an AJAX request to the server to capture the pit value
                 fetch('/capture_pit', {
                     method: 'POST',
@@ -77,8 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     //Just pushing the pit ID and then setting the dictionary value manually, will this work?
  
                 });
-                winCheckPits.push(pitNumber);
-                winCheckPits[pitNumber] = pitElement.textContent;
+                winCheckPits[pitNumber] = pit.textContent; // Make winCheckPits add all other board pits.
                 console.log(winCheckPits);
             });
         }
