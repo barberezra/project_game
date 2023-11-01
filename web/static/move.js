288c14d64a9be953067f77ddde87ba76c36dbbf1
Emilie Morocco <moroccoe@carleton.edu>
2023-11-01 10:22:13 -0500

    changes

--- web/static/move.js
+++ web/static/move.js
@@ -37,21 +37,13 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.pit').forEach(function(pit) {
         // Get the pit number from the data attribute
         var pitNumber = pit.getAttribute('data-pit');
-        
+
         if (pitNumber !== "7" && pitNumber !== "14" && pit.textContent !== "0") {
             pit.addEventListener('click', function() {
                 // Get the pit number from the data attribute
                 var pitNumber = pit.getAttribute('data-pit');
                 // Get list of all affected pits (from next pit to the last pit affected)
                 var pitRange = range(pitNumber, pit.textContent);
-
-                pitRange.forEach(pitIndex => {
-                    var pitElement = document.querySelector('.pit[data-pit="' + pitIndex + '"]');
-                    if (pitElement) {
-                        pitValues.push(pitElement.textContent);
-                    }
-                });
-
 
                 // Send an AJAX request to the server to capture the pit value
                 fetch('/capture_pit', {
@@ -77,7 +69,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     //Just pushing the pit ID and then setting the dictionary value manually, will this work?
  
                 });
-                winCheckPits.push(pitNumber);
                 winCheckPits[pitNumber] = pitElement.textContent;
                 console.log(winCheckPits);
             });
