bde97c1b5f8705756d29a4e8897f23e302827b84
TetraGenesis <santmonbarb@gmail.com>
2023-11-15 14:34:07 -0600

    trying to get db to work

--- app/src/components/Game/GameOver.js
+++ app/src/components/Game/GameOver.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import { Link, useLocation  } from 'react-router-dom';
+const mysql = require('mysql');
 
 // !! TODO !!: REFORMAT PAGE TO BETTER ILLUSTRATE WINNER
 const GameOver = () => {
     let location = useLocation();
     const playerOne = location.state.player1Score;
     const playerTwo = location.state.player2Score;
+    
+    if (playerOne > playerTwo) {
+        console.log("Player 1 wins");
+        winner = "Player 1";
+        loser = 'Player 2';
+    } else if (playerTwo > playerOne) {
+        console.log("Player 2 wins");
+        winner = 'Player 2';
+        loser = 'Player 1';
+    } else {
+        console.log("It's a tie");
+        var tie = true; // Handle a tie situation appropriately
+    }
 
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'thebestgame',
+        database: 'stuff'
+    });
+
+    connection.connect((err) => {
+        if (err) {
+          console.error('Error connecting to MySQL:', err);
+          return;
+        }
+        console.log('Connected to MySQL');
+      });
+
+      var incrementQuery = {query: `SELECT MAX(${winner}) AS maxWins, MAX(${loser}) AS maxLoserWins FROM scores`};
+      console.log(res);
+      fetch('/dbconnect', {
+          method: 'POST',
+          body: JSON.stringify({
+              incrementQuery: incrementQuery
+          }),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      })
+      .then(response => response.json())
+      .then(findResult => {
+          console.log('Response from server:', findResult);
+          if (findResult && findResult.result !== undefined) {
+              incWins = parseInt(findResult.result[0][0]) + 1;
+              sameVal = parseInt(findResult.result[0][1]);
+          }
+          var insertQuery = {query: `INSERT INTO scores (score, ${winner}, ${loser}) VALUES (%s, %s, %s)`, values: [dbString, incWins, sameVal]};
+          fetch('/dbconnect', {
+              method: 'POST',
+              body: JSON.stringify({
+                  insertQuery: insertQuery
+              }), 
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          })
+          .then(response => response.json())
+          .then(insertResult => {
+              console.log(insertQuery);
+              console.log('Response from server:', insertResult);
+              
+          })
+          .catch(error => {
+              console.error('Error:', error);
+          })
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      })
+
+    connection.end((err) => {
+    if (err) {
+        console.error('Error closing MySQL connection:', err);
+        return;
+    }
+    console.log('MySQL connection closed');
+    });
     return (
         <div>
             <h1>Player One Score: { playerOne }</h1>
---
+++ database/db-setup.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS stuff;
+USE stuff;
+CREATE TABLE IF NOT EXISTS scores (
+    gameID INT NOT NULL AUTO_INCREMENT,
+    score VARCHAR(30) NOT NULL,
+    numWins1 INT,
+    numWins2 INT,
+    tie INT,
+    PRIMARY KEY (gameID)
+);
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'thebestgame';
+CREATE USER IF NOT EXISTS 'leet'@'localhost' IDENTIFIED BY 'leetcode';
+GRANT ALL PRIVILEGES ON stuff.* to 'root'@'localhost' WITH GRANT OPTION;
+GRANT SELECT on stuff.scores to 'leet'@'localhost';
+INSERT INTO scores (score, numWins1, numWins2) VALUES ('0 : 0', 0, 0);
